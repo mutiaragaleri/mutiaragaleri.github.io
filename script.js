9a3087bc906150af445fffe36b2d503cd046f1f9
Ruslan Samuel <wawaheacafe@gmail.com>
2021-03-29 23:48:34 +0800

    Mengubah script

--- script.js
+++
@@ -1,7 +0,0 @@
-// Tombol Kirim
-const btnKirim = document.querySelector('#btnKirim');
-const btnLoading = document.querySelector('#btnLoading');
-btnKirim.addEventListener('click', function () {
-    this.classList.toggle('d-none');
-    btnLoading.classList.toggle('d-none');
-});
